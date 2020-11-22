67bee95860e3755e87022a7d64c3706836b38e4a
G.Felix <felix.grafschmidt@gmail.com>
2020-11-22 11:49:17 +0100

    mobile

---
+++ layouts/mobile.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+		<nuxt />
+	</div>
+</template>
+
+<script lang="ts">
+	// Vue basics
+	import { Component, Vue } from "nuxt-property-decorator";
+	@Component({ name: "mobile" })
+	export default class Mobile extends Vue {}
+</script>
+
+<style lang="scss">
+	@import "@/assets/styles.scss";
+	:root {
+		--color-primary: #d9ff00;
+		--color-secondary: #02fdff;
+	}
+</style>
---
+++ pages/mobile.vue
@@ -0,0 +1,92 @@
+<template>
+	<section class="columns is-mobile is-centered">
+		<div v-if="mode === 'character'" class="column">
+			<b-button expanded @click="mode = 'list'">Back</b-button>
+			<b-image v-for="(image, i) in character.images" :key="i" class="character-image" :src="image.src" />
+		</div>
+		<div v-else-if="mode === 'list'" class="column">
+			<b-button expanded @click="mode = 'collection'">Back</b-button>
+			<b-image v-for="character in list.characters" :key="character.id" class="character-image" :src="getMainImage(character)" @click.native="selectCharacter(character)" />
+		</div>
+		<div v-else class="column">
+			<b-button v-for="list in collection.lists" :key="list.id" expanded @click.prevent="selectList(list)">{{ list.name }}</b-button>
+		</div>
+	</section>
+</template>
+
+<script lang="ts">
+	// Vue basics
+	import { Component, Vue } from "nuxt-property-decorator";
+	import { Character, newCharacter } from "~/models/interfaces/Character";
+	import { Collection, newCollection } from "~/models/interfaces/Collection";
+	import { List, newList } from "~/models/interfaces/List";
+
+	@Component({ name: "mobile", layout: "mobile" })
+	export default class Mobile extends Vue {
+		collection: Collection = newCollection();
+		list: List = newList();
+		character: Character = newCharacter();
+		mode: string = "collection";
+
+		mounted() {
+			this.$buefy.dialog.prompt({
+				inputAttrs: {
+					placeholder: "Collection ID",
+				},
+				canCancel: false,
+				confirmText: "Load",
+				trapFocus: true,
+				onConfirm: (value) => {
+					this.$axios
+						.$get("loadCollection", {
+							params: {
+								id: value,
+							},
+						})
+						.then((response: Collection) => {
+							this.collection = response;
+						})
+						.catch((error) => {
+							console.error(error);
+						});
+				},
+			});
+		}
+
+		selectList(list: List) {
+			this.list = list;
+			this.changeMode("list");
+		}
+
+		selectCharacter(character: Character) {
+			this.character = character;
+			this.changeMode("character");
+		}
+
+		changeMode(mode: string) {
+			this.mode = mode;
+		}
+
+		getMainImage(character: Character) {
+			let imageResult;
+			character.images.forEach((image) => {
+				if (image.main) {
+					imageResult = image.src;
+				}
+			});
+			return imageResult;
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.columns {
+		margin: unset;
+	}
+	.button {
+		margin-top: 1rem;
+	}
+	.character-image {
+		margin-top: 1rem;
+	}
+</style>
