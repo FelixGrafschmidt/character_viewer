45604dc3b3d9e37ea1b7f5c15ac090aaa377f297
G.Felix <felix.grafschmidt@gmail.com>
2020-11-22 14:24:37 +0100

    directly load id

--- pages/mobile.vue
+++ pages/mobile.vue
@@ -1,14 +1,17 @@
 <template>
 	<section class="columns is-mobile is-centered">
 		<div v-if="mode === 'character'" class="column">
+			<div class="has-text-centered">{{ character.name }}</div>
 			<b-button expanded @click="mode = 'list'">Back</b-button>
 			<b-image v-for="(image, i) in character.images" :key="i" class="character-image" :src="image.src" />
 		</div>
 		<div v-else-if="mode === 'list'" class="column">
+			<div class="has-text-centered">{{ list.name }}</div>
 			<b-button expanded @click="mode = 'collection'">Back</b-button>
 			<b-image v-for="character in list.characters" :key="character.id" class="character-image" :src="getMainImage(character)" @click.native="selectCharacter(character)" />
 		</div>
 		<div v-else class="column">
+			<div class="has-text-centered">{{ collection.id }}</div>
 			<b-button v-for="list in collection.lists" :key="list.id" expanded @click.prevent="selectList(list)">{{ list.name }}</b-button>
 		</div>
 	</section>
@@ -23,35 +26,50 @@
 
 	@Component({ name: "mobile", layout: "mobile" })
 	export default class Mobile extends Vue {
-		collection: Collection = newCollection();
+		collection: Collection = newCollection("");
 		list: List = newList();
 		character: Character = newCharacter();
 		mode: string = "collection";
 
 		mounted() {
-			this.$buefy.dialog.prompt({
-				inputAttrs: {
-					placeholder: "Collection ID",
-				},
-				message: "",
-				canCancel: false,
-				confirmText: "Load",
-				trapFocus: true,
-				onConfirm: (value) => {
-					this.$axios
-						.$get("loadCollection", {
-							params: {
-								id: value,
-							},
-						})
-						.then((response: Collection) => {
-							this.collection = response;
-						})
-						.catch((error) => {
-							console.error(error);
-						});
-				},
-			});
+			if (this.$route.query.id) {
+				this.$axios
+					.$get("loadCollection", {
+						params: {
+							id: this.$route.query.id,
+						},
+					})
+					.then((response: Collection) => {
+						this.collection = response;
+					})
+					.catch((error) => {
+						console.error(error);
+					});
+			} else {
+				this.$buefy.dialog.prompt({
+					inputAttrs: {
+						placeholder: "Collection ID",
+					},
+					message: "",
+					canCancel: false,
+					confirmText: "Load",
+					trapFocus: true,
+					onConfirm: (value) => {
+						this.$axios
+							.$get("loadCollection", {
+								params: {
+									id: value,
+								},
+							})
+							.then((response: Collection) => {
+								this.collection = response;
+							})
+							.catch((error) => {
+								console.error(error);
+							});
+					},
+				});
+			}
 		}
 
 		selectList(list: List) {
