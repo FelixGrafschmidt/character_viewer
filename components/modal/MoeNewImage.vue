3c1d8b1d115e9b08c623ab8c1924c64232efd55f
G.Felix <felix.grafschmidt@gmail.com>
2021-05-13 11:38:04 +0200

    add image label Fixes #5

--- components/modal/MoeNewImage.vue
+++ components/modal/MoeNewImage.vue
@@ -1,18 +1,48 @@
 <template>
 	<form
-		class="rounded-2xl dark:bg-gray-700 bg-gray-400 dark:text-gray-100 text-gray-900 flex flex-col relative m-auto pb-10 z-20 max-h-[80vh] w-[40vw] items-center"
+		class="
+			rounded-2xl
+			dark:bg-gray-700
+			bg-gray-400
+			dark:text-gray-100
+			text-gray-900
+			flex flex-col
+			relative
+			m-auto
+			pb-10
+			z-20
+			max-h-[80vh]
+			w-[40vw]
+			items-center
+		"
 		@submit.prevent="src !== '' ? addImage() : undefined"
 	>
 		<img :src="src" alt="" class="m-8 max-h-[55vh]" />
-		<input
-			v-model="src"
-			v-autofocus
-			type="text"
-			class="block rounded-lg border text-gray-900 bg-gray-300 focus:outline-none h-8 mb-8 mx-12 w-80"
-		/>
+		<label class="mx-12">
+			<span>Image URL</span>
+			<input
+				v-model="src"
+				v-autofocus
+				type="text"
+				class="block rounded-lg border text-gray-900 bg-gray-300 focus:outline-none h-8 mb-8 w-80"
+		/></label>
 		<MoeButton text="Add Image" :class="{ 'cursor-not-allowed': src === '' }" class="m-auto" />
 		<div
-			class="top-1 right-1 dark:bg-red-500 dark:text-gray-900 bg-red-800 text-gray-100 h-6 w-6 text-center rounded-2xl ml-24 cursor-pointer absolute"
+			class="
+				top-1
+				right-1
+				dark:bg-red-500
+				dark:text-gray-900
+				bg-red-800
+				text-gray-100
+				h-6
+				w-6
+				text-center
+				rounded-2xl
+				ml-24
+				cursor-pointer
+				absolute
+			"
 			@click="$accessor.deactivateModal()"
 		>
 			X
