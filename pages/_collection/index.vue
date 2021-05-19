49443d10b057abc6b8ddc77d359ba4842098914f
G.Felix <felix.grafschmidt@gmail.com>
2021-05-19 17:59:00 +0200

    fix dark mode table borders

--- pages/_collection/index.vue
+++ pages/_collection/index.vue
@@ -44,10 +44,10 @@
 	</div>
 	<div v-else class="mb-20 mt-12 grid">
 		<div class="flex mb-2 sticky top-16 dark:bg-gray-600 bg-gray-400 z-[3] rounded font-extrabold text-lg">
-			<div class="py-2 w-2/5 border-r-2 text-center">ID</div>
-			<div class="py-2 w-2/5 border-r-2 border-l-2 text-center">Name</div>
-			<div class="py-2 w-1/6 border-r-2 border-l-2 text-center">Content</div>
-			<div class="py-2 w-1/6 border-l-2"></div>
+			<div class="py-2 w-2/5 dark:border-gray-800 border-r-2 text-center">ID</div>
+			<div class="py-2 w-2/5 dark:border-gray-800 border-r-2 border-l-2 text-center">Name</div>
+			<div class="py-2 w-1/6 dark:border-gray-800 border-r-2 border-l-2 text-center">Content</div>
+			<div class="py-2 w-1/6 dark:border-gray-800 border-l-2"></div>
 		</div>
 		<div class="">
 			<div
