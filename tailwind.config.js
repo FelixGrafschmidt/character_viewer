c3837cc7f1f4d2d6c27cd84618ed633477445554
G.Felix <felix.grafschmidt@gmail.com>
2021-05-19 12:51:22 +0200

    restore darkmode plugin to allow variatsn

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,7 +20,12 @@ module.exports = {
 		textColor: ["dark", "dark-hover", "dark-active", "hover", "responsive"],
 		scrollbar: ["dark"],
 	},
-	plugins: [require("@tailwindcss/forms"), require("tailwind-scrollbar"), require("tailwindcss-typography")],
+	plugins: [
+		require("tailwindcss-dark-mode")(),
+		require("@tailwindcss/forms"),
+		require("tailwind-scrollbar"),
+		require("tailwindcss-typography"),
+	],
 	purge: {
 		// enable remove unused CSS only in production
 		enabled: process.env.NODE_ENV === "production",
