0ccee91b3ce0b5d5676a680fef4e308b7a4b845a
G.Felix <felix.grafschmidt@gmail.com>
2021-06-03 16:59:08 +0200

    fullscreenimg size

--- components/modal/MoeFullscreenImg.vue
+++ components/modal/MoeFullscreenImg.vue
@@ -6,20 +6,21 @@
 				invisible: $vxm.main.characterStore.character.images.length <= 1,
 			}"
 			@click.native.stop="$vxm.main.characterStore.previousImage"
-			class="h-20 w-20 !rounded-full text-5xl ml-2"
+			class="h-20 !rounded-full text-5xl ml-2 w-20 max-w-[7%]"
 			icon="fas fa-angle-left"
 		/>
-		<figure class="w-full h-full justify-center flex">
-			<img :src="src" class="max-h-screen max-w-[80vw] h-full w-full" />
-		</figure>
-
+		<div class="max-w-[86%]">
+			<figure class="justify-center flex">
+				<img :src="src" class="max-h-screen max-w-[80vw] h-full w-screen object-contain" />
+			</figure>
+		</div>
 		<MoeButtonLight
 			@click.native.stop="$vxm.main.characterStore.nextImage"
 			:class="{
 				visible: $vxm.main.characterStore.character.images.length > 1,
 				invisible: $vxm.main.characterStore.character.images.length <= 1,
 			}"
-			class="h-20 w-20 !rounded-full text-5xl mr-2"
+			class="h-20 w-20 !rounded-full text-5xl mr-2 max-w-[7%]"
 			icon="fas fa-angle-right"
 		/>
 	</div>
